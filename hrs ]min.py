50dd6d811354fcfdae4850fc6e135fe460e6e8ba
Carlosfelipe00000 <69310434+Carlosfelipe00000@users.noreply.github.com>
2025-01-08 15:52:21 -0300

    Add files via upload

---
+++ hrs ]min.py
@@ -0,0 +1,45 @@
+import time
+while True:
+    try:
+        h = int(input('Digite quantas horas você quer:'))
+        print()
+        print('continuando')
+        print()
+        break
+    except ValueError: 
+        print('Digite um número!')
+while True:
+    try:
+        m = int(input('Digite quantos minutos você quer:'))
+        if m > 60:
+            print('Digite um número entre 1 e 60!')
+        else:
+            print()
+            print('continuando')
+            print()
+            break
+    except ValueError: 
+        print('Digite um número!')
+while True:
+    try:
+        s = int(input('Digite quantos segundos você quer:'))
+        if s > 60:
+            print('Digite um número entre 1 e 60!')
+        else:
+            print()
+            c = input('digite qualquer tecla para continuar...')
+            print()
+            break
+    except ValueError: 
+        print('Digite um número!')
+        
+print(f'Você digitou: [{h:02}h:{m:02}m:{s:02}s]')
+print()   
+input('digite uma tecla para continuar...')
+print()
+print('transformando em segundos...')
+time.sleep(3)
+
+ss = h*3600 + m*60 + s
+
+print(f'São ao todo {ss} segundos')
